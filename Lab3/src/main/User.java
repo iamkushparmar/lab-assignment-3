6d205cdf8b3440a530ba4202c025730324b0392f
iamkushparmar <parmar.ku@northeastern.edu>
2024-02-10 23:52:55 -0500

    initial commit

---
+++ Lab3/src/main/User.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author kushp
+ */
+public class User {
+    String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+}
